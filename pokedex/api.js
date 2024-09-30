2d8859405576d430dd065806992f33e92581b1bb
AnaClaraprisma <an4claracouto@gmail.com>
2024-09-30 10:59:35 -0300

    motrar na tela

---
+++ pokedex/api.js
@@ -0,0 +1,45 @@
+const url = "https://pokeapi.co/api/v2/pokemon"
+
+function carregarPokemon(){
+    fetch(url)
+.then((response) => response.json())
+.then(data =>{
+    const listaPokemons = data.results;
+
+    listaPokemons.forEach(pokemon => {
+        fetch(pokemon.url)
+        .then(response =>response.json())
+        .then(pokemonData =>{
+           /* const pkmType = pokemonData.types.map(typeInfo =>{
+                typeInfo.type.name.join(", ")
+            })*/
+            document
+            .getElementById("pokedex")
+            .innerHTML += `
+
+<div class="pokemom">
+<div class="cx1">
+     <section class="cabeca">
+     ${pokemonData.types.map(typeInfo => `
+     <span class="grama">${pkmType}</span>`)}
+   
+    <span class="veneno">poison</span>
+</section>
+<span class="id">#0001</span>
+</div>
+<div class="cx2">
+    <section class="information">
+    <h1>Bulbasaur</h1>
+    <p>A strange seed was planted on its back at birth. the plant sprouts and grows with this pokémon.</p>
+</section>
+<img class="bulbasaur" src="imagens/1_1.png" alt="bubassauro">
+</div>
+</div>`
+        })
+    })
+        
+    });;
+} 
+carregarPokemon();
+
+let main =document.getelementById(`pokedex`)
